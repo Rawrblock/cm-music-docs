f01c4c05c3a19e53cce3c1189454538e4febac51
rawrblock <799389926@qq.com>
2022-07-14 11:09:21 +0800

    feat: readme

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,8 +2,8 @@ import { defineUserConfig,defaultTheme } from 'vuepress'
 
 export default defineUserConfig({
   lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
+  title: '音乐APP全栈开发',
+  description: '实战项目',
   port: 8081,
   theme: defaultTheme({
     logo: "/images/logo.png"
